026632c7a6f152ffbb65f3f5cd52b0377f0d5700
aimrane <aimrane.essakhi@gmail.com>
2024-01-15 12:08:00 +0100

    tt

---
+++ taxeTnbMS/src/main/java/com/example/taxe/entities/Terrain.java
@@ -0,0 +1,25 @@
+package com.example.taxe.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Terrain {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	private int surface;
+
+	@ManyToOne
+	@Transient
+	private Proprietaire proprietaire;
+
+	@ManyToOne
+	private Categorie categorie;
+
+}
